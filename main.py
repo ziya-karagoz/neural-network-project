09149dff650eedf83f1c90bf1b8b40da513dfd6d
Ziya <ziyakaragoz42@gmail.com>
2021-12-29 16:36:52 +0300

    hersey bitti

--- main.py
+++ main.py
@@ -7,9 +7,15 @@ from sklearn.model_selection import train_test_split
 hap = pd.read_csv("./data.csv")
 hap.reset_index(drop=True, inplace=True)
 
-x = hap.iloc[:, 2:]
-y = hap.iloc[:, 1]
-y = np.array([1 if i >= 5 else 0 for i in y]).reshape(149, 1)
+hapTestX = hap.iloc[130:, 2:]
+hapTestXCountry = hap.iloc[130:, 0]
+hapTestXCountry = np.array(hapTestXCountry)
+hapTestY = hap.iloc[130:, 1]
+hapTestY = np.array([1 if i >= 5 else 0 for i in hapTestY])
+
+x = hap.iloc[:130, 2:]
+y = hap.iloc[:130, 1]
+y = np.array([1 if i >= 5 else 0 for i in y]).reshape(130, 1)
       
 #  Her Satır bir eğitim örneği, her sütun bir özellik  [X1, X2, X3]
 giris_seti = np.array((x.values), dtype=float) # (149, 8)
@@ -34,7 +40,7 @@ for epoch in range(700):
     XW = np.dot(girisler, agirliklar)+ bias
     z = sigmoid(XW)
     error = z - labels
-    print(error.sum())
+    #print(error.sum())
     dcost = error
     dpred = sigmoid_turevi(z)
     z_del = dcost * dpred
@@ -43,8 +49,19 @@ for epoch in range(700):
     for num in z_del:
         bias = bias - lr*num
 
-bir_data = np.array(x.values[23])
-sonuc = sigmoid(np.dot(bir_data, agirliklar) + bias)
+dogruluk = 0
+for i in range(18):
+    
+    bir_data = np.array(hapTestX.values[i])
+    sonuc = sigmoid(np.dot(bir_data, agirliklar) + bias)
 
-# sonucu ekrana yazdırma
-print(sonuc)
+    if sonuc>0.5:
+        sonuc = 1
+    else:
+        sonuc = 0
+    
+    if sonuc == hapTestY[i]:
+        dogruluk = dogruluk +1
+
+    print("Agin Bulduğu ",hapTestXCountry[i],": \t", sonuc, " ---- Beklenen: ", hapTestY[i])
+print("Dogruluk Orani: %", (dogruluk/18) * 100)
